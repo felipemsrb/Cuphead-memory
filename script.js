e57c1e40a12720b5ca429fcdbdea268837cedcfb
Felipe Mota <felipe.msrb@gmail.com>
2022-06-25 12:17:59 +0100

    adicionando as funcoes para o funcionamento correto das cartas

--- script.js
+++ script.js
@@ -6,11 +6,42 @@ let firstCard, secondCard;
 
 function flipCard() {
     this.classList.add('flip');
+    
+    
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+
+}
+
+function checkForMatch() {
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+    
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(()=> {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    },1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-if(!hasFlippedCard){
-    hasFlippedCard = true;
-    firstCard = this;
-}
+
+
