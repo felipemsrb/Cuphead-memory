68cf152ac7f34b0f6d43b42e1d55072b236e4189
Felipe Mota <felipe.msrb@gmail.com>
2022-06-25 16:32:35 +0100

    adicionando funcao resetBoard()

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let lockBoard = false;
 
 function flipCard() {
     if (lockBoard) return;
+    if(this === firstCard) return;
 
     this.classList.add('flip');
     
@@ -18,7 +19,7 @@ function flipCard() {
     }
     
     secondCard = this;
-    hasFlippedCard = false;
+ 
     checkForMatch();
 
 }
@@ -35,6 +36,8 @@ function checkForMatch() {
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
@@ -42,8 +45,13 @@ function unflipCards() {
     setTimeout(()=> {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-        lockBoard = false;
+        resetBoard();
     },1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
